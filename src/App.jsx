712dc7dc2de0540f3318dfc2aabd5ac3858b31d2
Ihfadzh D <ihfadzhdiinaka7@gmail.com>
2025-02-20 13:20:12 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { requestToGroqAI } from './utils/groq';
+import {Light as SyntaxHighlight} from "react-syntax-highlighter"
+import {darcula} from "react-syntax-highlighter/dist/cjs/styles/prism"
+import './App.css';
+
+function App() {
+    const [data, setData] = useState("");
+
+    const handleSubmit = async () => {
+      const ai = await requestToGroqAI(content.value);
+      setData(ai);
+    }
+    return (
+      <main className='flex flex-col min-h-[80vh] justify-center item-center max-w-xl w-full mx-auto'>
+        <h1 className='text-4xl text-green-600'>Cet CPT</h1>
+        <form className='flex flex-col gap-4 py-4' w-full>
+          <input 
+          placeholder='Malu bertanya sesat di jalan...'
+          className='py-2 px-4 text-md rounded-md'
+          id="content"
+          type="text"
+          />
+          <button 
+          onClick={handleSubmit}
+          type="button"
+          className='bg-green-600 py-2 px-4 font-bold text-white rounded-md'>Kirim</button>
+        </form>
+        <div className='bg-green-200 max-w-xl w-full mx-auto'>
+          {data ? (
+        <SyntaxHighlight language='swift' style={darcula} wrapLongLines={true}>
+          {data}
+        </SyntaxHighlight>
+          ) : null}
+        </div>
+      </main>
+    )
+}
+
+
+export default App
